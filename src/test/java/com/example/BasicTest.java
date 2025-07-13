db7e54db64bd36e47117f757d5eb6d1c21fc3830
digitalkfmw <digitalk.fmw@gmail.com>
2025-07-13 12:20:14 +0000

    Initial mywebapp

---
+++ src/test/java/com/example/BasicTest.java
@@ -0,0 +1,7 @@
+package com.example;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class BasicTest {
+  @Test void testTrue() { assertTrue(true); }
+}
+
